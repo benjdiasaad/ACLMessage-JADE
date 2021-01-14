469e984ef0a1b0ee2fadd9481405a6e821767ad0
benjdia <cravatas90@gmail.com>
2021-01-14 17:10:52 +0100

    Generate the setup method for sender

--- CommunicationJadeAgent/src/msg/Sender.java
+++ CommunicationJadeAgent/src/msg/Sender.java
@@ -1,5 +1,28 @@
 package msg;
 
-public class Sender {
+import jade.core.AID;
+import jade.core.Agent;
 
+import jade.core.behaviours.OneShotBehaviour;
+import jade.lang.acl.ACLMessage;
+
+public class Sender extends Agent {
+	   
+	@Override
+	protected void setup(){
+		
+		addBehaviour(new OneShotBehaviour() {
+			
+			@Override
+			public void action() {
+				ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+				//set the message content to send
+				msg.setContent("Bonjour Saad");
+				//add a the message receiver
+				msg.addReceiver(new AID("Receiver",AID.ISLOCALNAME));
+				//send the message
+				send(msg);
+			}
+		});	
+	}
 }
